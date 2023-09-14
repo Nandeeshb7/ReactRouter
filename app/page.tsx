65e92b8160eac9a7d150ef0a652a86ffde202bf4
Fervid smart <nandeesh.b@fervid.com>
2023-09-14 10:33:11 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+// pages/_app.tsx
+import "./globals.css";
+import type { AppProps } from "next/app";
+import Layout from "./layout";
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
+}
+export default MyApp;
